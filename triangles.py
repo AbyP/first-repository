5084200af025d26241bb3ba536c4783c0b086108
Aby <apachon@ucvts.org>
2018-12-17 17:39:15 +0000

    Updated and added triangles.py

--- triangles.py
+++ triangles.py
@@ -1,7 +1,11 @@
 import math
-a = int(input("Give me a number for side 1 "))
-b = int(input("Give me a number for side 2 "))
-c = a**2 + b**2
-c = math.sqrt(c)
-print(c)
+def findhypotenuse():
+    a = int(input("Give me a number for side 1 "))
+    b = int(input("Give me a number for side 2 "))
+    c = a**2 + b**2
+    c = math.sqrt(c)
+    print(c)
+    return (c)
+
+findhypotenuse()
 
